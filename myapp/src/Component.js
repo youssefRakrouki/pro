0d6e32e9d39dde236bd09a45f511baf84d993be6
hamza.rakrouki <hamzarakrouki>
2019-07-16 13:47:04 +0100

    second commit

---
+++ myapp/src/Component.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Image from'./a.jpg';
+import Image1 from'./img/b.jpg';
+import style from'./style.css';
+function Component () {
+
+    return (
+        <div>
+            <div>
+    
+<div style={{border:"solid 1px black",maxWidth:"100vw" }} >
+<h1 className="title red" >Mytitle</h1>
+<br/>
+
+<img src={Image}/> 
+<br />
+<img src={Image1} />
+
+
+</div>
+</div>
+    
+    </div>
+    )
+}
+
+export default Component
